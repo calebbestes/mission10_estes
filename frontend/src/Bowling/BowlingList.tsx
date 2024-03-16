f8bb4726b24862ee935e5095401292d65ea4150d
calebbestes <131561201+calebbestes@users.noreply.github.com>
2024-03-16 11:40:58 -0600

    comments in BowlingList

--- frontend/src/Bowling/BowlingList.tsx
+++ frontend/src/Bowling/BowlingList.tsx
@@ -1,15 +1,27 @@
 import { useEffect, useState } from 'react';
 import { Bowling } from '../types/Bowling';
 
+// BowlingList is a functional component (declared as a funciton and doesn't extend from a class)
 function BowlingList() {
+    // declares bowling data as a state variable (which is a piece of data 
+    // managed by a component that changes over time), and its setter function,
+    // using the useState hook (which sets it initially to an empty array of type bowling)
   const [bowlingData, setBowlingData] = useState<Bowling[]>([]);
+//   use effect performs the side effects of functional components
   useEffect(() => {
+    // makes an HTTP Get request the website
     const fetchBowlingData = async () => {
+      // rsp holds the raw JSON
       const rsp = await fetch('http://localhost:5079/bowling');
+      //   b holds the parsed JSON
       const b = await rsp.json();
+      //Updates the state variable bowlingData with b
       setBowlingData(b);
     };
+    //calls the function
     fetchBowlingData();
+    //In React's useEffect hook, if you provide an empty dependency array 
+    // as the second argument, React will execute the effect only after the initial render and never re-run it
   }, []);
 
   return (
@@ -32,8 +44,11 @@ function BowlingList() {
           </tr>
         </thead>
         <tbody>
+            {/* checks if the bowlingData is an array */}
           {Array.isArray(bowlingData) &&
+        //   maps each element of the bowlingData array to a table row 
             bowlingData.map((b) => (
+                // b is the json array declared above. 
               <tr key={b.bowlerId}>
                 <td>{b.bowlerFirstName}</td>
                 <td>{b.bowlerMiddleInit}</td>
